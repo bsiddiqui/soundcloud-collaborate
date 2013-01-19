1080d985976b9fc2ef7c0357268dec58a89a049a
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-01-19 14:27:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayedbyMeHh::Application.config.secret_token = 'd8f0050aa13ce8d4b50c67327843aa317e57d1cebe01f9e8a2d20a61cb9bff4576cc829756ed0b9b69cbb7be23cb78a850ad9c041c8c79d8cef9ab01c4741227'
