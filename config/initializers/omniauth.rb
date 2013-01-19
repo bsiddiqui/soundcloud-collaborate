2389d0962933a9c6661692da5eb7074a91cf0a64
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-01-19 16:36:53 -0500

    copied stuff from old playedby.me

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '457101824352373', 'c7e34e17d40621164fce9f66c0419406'
+  			  :scope => 'email', :display => 'popup'
+end
