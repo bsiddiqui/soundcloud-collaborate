0d73c4faca6f684817faaeefd249bcca9bd88c03
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-01-22 15:18:38 -0500

    fixed parties hosted alert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,16 +10,19 @@ class UsersController < ApplicationController
 
   # queries database for parties associated with user to display on their homepage
   def show
-  @parties1 = PartyProfile.new
-  @parties1 = PartyProfile.find_all_by_host(current_user.id)
 
- # @guest = Guest.find_all_by_user_id(current_user.id)
-  #  unless @guest.empty?
- #     params[:guest].each do |i|
-   #     @parties2 = PartyProfile.find_all_by_id(guest.party_profile_id) 
-   #   @parties_guest = PartyProfiles.find_all_by_id(@guest.party_profile_id)
-    #  end
-   # end 
+    @parties_hosted = PartyProfile.new
+    @parties_hosted = PartyProfile.find_all_by_host(current_user.id)
+    if @parties_hosted.empty?
+      flash.now[:alert] = "You have not thrown any parties. Start a party?"
+    end
+   # @guest = Guest.find_all_by_user_id(current_user.id)
+    #  unless @guest.empty?
+   #     params[:guest].each do |i|
+     #     @parties2 = PartyProfile.find_all_by_id(guest.party_profile_id) 
+     #   @parties_guest = PartyProfiles.find_all_by_id(@guest.party_profile_id)
+      #  end
+     # end 
   end
 
   def index
