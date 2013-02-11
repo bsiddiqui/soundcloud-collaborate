bc7db0e5388916bdbd7f5634fbf1086c9d0eb154
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-02-11 00:26:42 -0500

    added Beta table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124174320) do
+ActiveRecord::Schema.define(:version => 20130211052622) do
+
+  create_table "beta", :force => true do |t|
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "guests", :force => true do |t|
     t.string   "user_id"
