0be9355f487ef62890fcc6e6f68db4bccefbd5af
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-02-18 15:29:50 -0500

    fixed email/added imgs

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,9 @@
 ActionMailer::Base.smtp_settings = {
 	:address => "smtp.gmail.com",
-	:port => 587,
-	:domain => "gmail.com",
-	:user_name => "shrlyai.sa",
-	:password => "Aixueli1029",
+	:port => 587 ,
+	:domain => "playedby.me",
+	:user_name => "team@playedby.me",
+	:password => "!PBM@2013",
 	:authentication => "plain",
 	:enable_starttls_auto => true
-
 }
